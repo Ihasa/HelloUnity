d05f9019ca23dc80cf74d22c85fecd9d9658a858
Toshihiko Asahi <lovingcat222@gmail.com>
2018-07-15 22:24:34 +0900

    素敵なメッセージテキスト

---
+++ Assets/Scripts/Texter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Texter : MonoBehaviour {
+    private string dispText;
+    public float duration;
+    private UnityEngine.UI.Text text;
+    private float times = 0;
+    private int chars = 0;
+    private int chars_pre = 0;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<UnityEngine.UI.Text>();
+        dispText = text.text;
+        text.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (times < maxTime())
+        {
+            times += Time.deltaTime;
+            chars = (int)(times / duration);
+            if (chars != chars_pre)
+            {
+                text.text = dispText.Substring(0, chars);
+                chars_pre = chars;
+            }
+        } else if (times < maxTime()+2.0f)
+        {
+            times += Time.deltaTime;
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+    private float maxTime()
+    {
+        return dispText.Length * duration;
+    }
+}
