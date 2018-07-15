fb90c4d35fe7dc080fa923e5a88edac5a06e4ede
Toshihiko Asahi <lovingcat222@gmail.com>
2018-07-16 01:27:09 +0900

    スペースキーでプレイヤーがちょうど4.0だけジャンプ RigidBody.velocityはこういう場合以外は直接変更しない dragはよく分からない

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,12 +15,30 @@ public class PlayerController : MonoBehaviour
 
     private int Count { get; set; }
 
+    private float maxY;
+
     void Start()
     {
         Debug.Log("" + Physics.gravity);
         rb = GetComponent<Rigidbody>();
         Count = 0;
         SetCount();
+        maxY = this.gameObject.transform.position.y;
+    }
+
+    private void Update()
+    {
+        bool pressed = Input.GetKeyDown("space");
+
+        if (pressed)
+        {
+            float speedY = Mathf.Sqrt(2.000f * (-Physics.gravity.y) * 4.000f); /* 重力と揚力はともに上が+ */
+            rb.velocity = new Vector3(rb.velocity.x, speedY, rb.velocity.z);
+        }
+
+        float currentY = this.gameObject.transform.position.y;
+        maxY = (currentY > maxY) ? currentY : maxY;
+        countText.text = maxY + "";
     }
 
     void FixedUpdate()
