cbf7dc4917da493835ab7127c14d3f7d0605f5c6
Toshihiko Asahi <lovingcat222@gmail.com>
2018-07-29 23:45:39 +0900

    着弾時にパーティクルを射出 デザインはてきとう

---
+++ Assets/Scripts/GroundedParticle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedParticle : MonoBehaviour {
+
+    public ParticleSystem particle;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        particle.transform.position = collision.transform.position;
+        particle.Emit(10);
+    }
+}
