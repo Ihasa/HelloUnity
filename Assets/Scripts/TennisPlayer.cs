991b37829bc6e5f7fd2b0b77d701339d30caae31
Toshihiko Asahi <lovingcat222@gmail.com>
2018-08-22 00:05:21 +0900

    打球、 Aim、移動でコントローラを分けて自動操作を実装
    スペースキーでボール複製を一旦削除
    打ち合いの相手を追加

--- Assets/Scripts/TennisPlayer.cs
+++ Assets/Scripts/TennisPlayer.cs
@@ -11,93 +11,211 @@ public class TennisPlayer : MonoBehaviour {
     public Vector3 aimVia;
     public float spinA;
     public float spinB;
+    public bool isAutoShot;
+    public bool isAutoMove;
+    public bool isAutoAim;
 
-    private IController controller;
+    private IShotController shotController;
+    private IMoveController moveController;
+    private IAimController aimController;
     private Projectiler ballController;
     private AudioSource shotSound;
     private AudioSource runSound;
-    private ControllerState cStatePrev;
+    private Vector2 cStateMovePrev;
 
     // Use this for initialization
     void Start () {
-        controller = new ArrowController();
+        shotController = isAutoShot ? (IShotController)new AutoShotController(gameObject, ballObject.GetComponent<Rigidbody>()) : (IShotController)new KeyShotController();
+        moveController = isAutoMove ? (IMoveController)new AutoMoveController(gameObject, ballObject.GetComponent<Rigidbody>()) : (IMoveController)new KeyMoveController();
+        aimController = isAutoAim ? (IAimController)new AutoAimController(gameObject) : (IAimController)new MouseAimController(mainCamera);
+
         ballController = ballObject.GetComponent<Projectiler>();
         AudioSource[] clips = GetComponents<AudioSource>();
         runSound = clips[0];
         shotSound = clips[1];
-        cStatePrev = new ControllerState();
+        cStateMovePrev = new Vector2();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit[] hits = Physics.RaycastAll(ray);
-        foreach (RaycastHit hit in hits)
-        {
-            if (hit.collider.CompareTag("Ground"))
-            {
-                aim = hit.point;
-                aimMark.transform.position = aim;
-            }
-        }
+        Vector3 cStateAim = aimController.GetAim();
+        aim = cStateAim;
+        aimMark.transform.position = aim;
 
-        ControllerState cState = controller.GetControllerState();
-        this.gameObject.transform.position += new Vector3(cState.direction.x, 0, cState.direction.y) * runSpeed * Time.deltaTime;
-        if(cStatePrev.direction == Vector2.zero && cState.direction != Vector2.zero)
+
+        Vector2 cStateMove = moveController.GetDirection();
+        this.gameObject.transform.position += new Vector3(cStateMove.x, 0, cStateMove.y) * runSpeed * Time.deltaTime;
+        if(cStateMovePrev == Vector2.zero && cStateMove != Vector2.zero)
         {
             runSound.Play();
-        } else if(cStatePrev.direction != Vector2.zero && cState.direction == Vector2.zero)
+        } else if(cStateMovePrev != Vector2.zero && cStateMove == Vector2.zero)
         {
             runSound.Stop();
         }
-        if (cState.shotA)
+        cStateMovePrev = cStateMove;
+
+        ShotControllerState cStateShot = shotController.GetControllerState();
+        if (cStateShot.shotA)
         {
             ballController.projectile(aim, aimVia, spinA);
             shotSound.Play();
-        } else if (cState.shotB)
+        } else if (cStateShot.shotB)
         {
             ballController.projectile(aim, aimVia, spinB);
             shotSound.Play();
-        } else if (cState.toss)
+        } else if (cStateShot.toss)
         {
             toss(3);
         }
-        cStatePrev = cState;
     }
 
     public void toss(float height)
     {
-        ballObject = Instantiate(ballObject, this.transform.position + Vector3.forward + Vector3.up, Quaternion.identity);
-        ballController = ballObject.GetComponent<Projectiler>();
         ballController.jump(height);
     }
 
 }
 
-struct ControllerState
+struct ShotControllerState
 {
-    public Vector2 direction;
     public bool shotA;
     public bool shotB;
     public bool toss;
 }
 
-interface IController
+interface IShotController
 {
-    ControllerState GetControllerState();
+    ShotControllerState GetControllerState();
 }
 
-class ArrowController : IController
+class KeyShotController : IShotController
 {
-    public ControllerState GetControllerState()
+    public ShotControllerState GetControllerState()
     {
-        ControllerState ret = new ControllerState();
-        ret.direction.x = Input.GetAxis("Horizontal");
-        ret.direction.y = Input.GetAxis("Vertical");
+        ShotControllerState ret = new ShotControllerState();
         ret.shotA = Input.GetMouseButtonDown(0);
         ret.shotB = Input.GetMouseButtonDown(1);
         ret.toss = Input.GetKeyDown("space");
         return ret;
     }
 }
+
+class AutoShotController : IShotController
+{
+    private GameObject player;
+    private Rigidbody ballRigidbody;
+    private ShotControllerState prev;
+    public AutoShotController(GameObject player, Rigidbody ballRigidbody)
+    {
+        this.player = player;
+        this.ballRigidbody = ballRigidbody;
+        prev = new ShotControllerState();
+    }
+    public ShotControllerState GetControllerState()
+    {
+        ShotControllerState cState = new ShotControllerState();
+        if(Mathf.Sign(ballRigidbody.transform.position.z) == Mathf.Sign(player.transform.position.z) && 
+           Mathf.Sign(ballRigidbody.velocity.z) == Mathf.Sign(player.transform.position.z))
+        {
+            if (Mathf.Abs(ballRigidbody.transform.position.z) > 6.4f)
+            {
+                cState.shotA = !prev.shotA;
+            }
+        }
+        prev = cState;
+        return cState;
+    }
+}
+
+interface IMoveController
+{
+    Vector2 GetDirection();
+}
+
+class KeyMoveController : IMoveController
+{
+    public Vector2 GetDirection()
+    {
+        Vector2 direction = new Vector2();
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+        return direction;
+    }
+}
+
+class AutoMoveController : IMoveController
+{
+    private GameObject player;
+    private Rigidbody rb;
+    public AutoMoveController(GameObject player, Rigidbody rb)
+    {
+        this.player = player;
+        this.rb = rb;
+    }
+    public Vector2 GetDirection()
+    {
+        Vector3 pos;
+        if (Mathf.Sign(rb.velocity.z) == Mathf.Sign(player.transform.position.z) && rb.velocity.z != 0)
+        {
+            pos = rb.transform.position;
+        } else
+        {
+            pos = new Vector3(0, 0, 13 * Mathf.Sign(player.transform.position.z));
+        }
+        Vector3 diff = (pos - player.transform.position).normalized;
+
+        Vector2 res;
+        if ((player.transform.position - pos).magnitude > 1)
+        {
+            res = new Vector2(diff.x, diff.z);
+        }
+        else
+        {
+            res = Vector2.zero;
+        }
+        return res;
+    }
+}
+
+interface IAimController
+{
+    Vector3 GetAim();
+}
+
+class MouseAimController : IAimController
+{
+    private Camera mainCamera;
+    public MouseAimController(Camera mainCamera)
+    {
+        this.mainCamera = mainCamera;
+    }
+    public Vector3 GetAim()
+    {
+        Vector3 result = Vector3.zero;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] hits = Physics.RaycastAll(ray);
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.CompareTag("Ground"))
+            {
+                result = hit.point;
+            }
+        }
+        return result;
+    }
+}
+
+class AutoAimController : IAimController
+{
+    private GameObject player;
+    public AutoAimController(GameObject player)
+    {
+        this.player = player;
+    }
+    public Vector3 GetAim()
+    {
+        return new Vector3(Random.Range(-2,2), 0, Mathf.Sign(player.transform.position.z) * -7);
+    }
+}
+
 
