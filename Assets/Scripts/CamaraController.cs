9e82673c7d93a21eb91a5fb5fab3b7cd3a622dd2
Toshihiko Asahi <lovingcat222@gmail.com>
2018-08-23 23:36:02 +0900

    シフトキーでボールを見る

--- Assets/Scripts/CamaraController.cs
+++ Assets/Scripts/CamaraController.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class CamaraController : MonoBehaviour {
 
     public GameObject player;
+    public GameObject ball;
     public float height;
-
+    private bool lookBall;
+    
 	// Use this for initialization
 	void Start () {
 	}
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            lookBall = true;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            lookBall = false;
+        }
+    }
+
     // Update is called once per frame
     void LateUpdate () {
-        GameObject ball = player.GetComponent<TennisPlayer>().ballObject;
-        this.transform.position = new Vector3(player.transform.position.x, height, player.transform.position.z);
+        Vector3 camPos = new Vector3(player.transform.position.x, height, player.transform.position.z);
+        this.transform.position = camPos;
+
+        Vector3 lookAt;
+        if (lookBall)
+        {
+            lookAt = ball.transform.position;
+        } else
+        {
+            lookAt = camPos + Vector3.forward;
+        }
+        this.transform.LookAt(lookAt);
 	}
 }
